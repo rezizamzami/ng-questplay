ce8f12fbc53ab8fefc10deb2f3a35c8fd6a7e534
rezizamzami <rezizamzami2502@gmail.com>
2025-09-18 02:41:21 +0700

    #setting-up

--- campaigns/standalone-quests/setting-up/contracts/HelloGuardian.sol
+++ campaigns/standalone-quests/setting-up/contracts/HelloGuardian.sol
@@ -1,4 +1,4 @@
-// SPDX-License-Identifier: MIT
+ // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.19;
 
 contract HelloGuardian {
@@ -6,7 +6,7 @@ contract HelloGuardian {
     /// @return "Hello Guardian"
     function hello() external pure returns (string memory) {
         // UNCOMMENT THE NEXT LINE TO MAKE THE TEST PASS
-        // return "Hello Guardian";
+        return "Hello Guardian";
     }
 
 }
